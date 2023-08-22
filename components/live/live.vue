1e32e0bc9a4e1715b2d1d132d4bfe4b14dbdd34e
quanhao <2542568096@qq.com>
2023-08-22 19:51:12 +0800

    这是第三次提交

---
+++ components/live/live.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<scroll-view scroll-y  style="height: 300rpx;" >
+			<view v-for="(item,index) in 4" :key="index" class="row" >
+					<view :class="['boder_liwu_left','row']" style="align-items: center;">
+						<image src="../../static/phone.png" class="text_yuan" mode=""></image>
+						<view class="direct_Head_Left_Right" style="width: 150rpx;">
+							<text style="display: block;color: white;">昵称</text>
+							<text style="display: block;color: white;">送皇冠</text>
+						</view>
+						<view class="text_yuan" style="font-size: 80rpx;line-height: 80rpx;margin-right: 10rpx;">
+							<svg class="icon bottom_Icon" aria-hidden="true">
+								<use xlink:href="#icon-liwu1"></use>
+							</svg>
+						</view>
+					</view>
+					<view class="boder_liwu_right">
+						x1
+					</view>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"live",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+.row{
+	flex-direction: row;
+}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		
+		<scroll-view :scroll-x="true" style="white-space: nowrap; width:120rpx">
+			<view style="display: flex;">
+				<view class="div" v-for="(item,index) in 12" :key="index">
+					{{index}}
+				</view>
+			</view>
+		</scroll-view>
+		<button><navigator url="../nvues/nvues" open-type="navigate">传送</navigator></button>
 	</view>
 </template>
 
@@ -17,6 +24,11 @@
 	}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.div{
+	width: 100rpx;
+	height: 100rpx;
+	background-color: aqua;
+	margin-right: 10rpx;
+}
 </style>
